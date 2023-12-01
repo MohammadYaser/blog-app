c9f0a27b507f67469db7df319ed560c06b8b27ca
MohammadYaser <yasersafi19@outlook.com>
2023-12-01 22:26:38 +0430

    change photo attribute datatype to string

--- db/migrate/20231201164130_create_users.rb
+++ db/migrate/20231201164130_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.text :photo
+      t.string :photo
       t.text :bio
       t.integer :post_counter
 
