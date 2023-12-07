1ef434bb3cdaa66155dced087c25c98fcb2d55cf
MohammadYaser <yasersafi19@outlook.com>
2023-12-07 17:17:48 +0430

    add rspec unit test for like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,4 +1,12 @@
+# spec/models/like_spec.rb
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+  end
 end
