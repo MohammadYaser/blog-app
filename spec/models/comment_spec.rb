a62ab4d3abb2ce42e1c980d215feaf69b819cc88
MohammadYaser <yasersafi19@outlook.com>
2023-12-07 17:14:39 +0430

    write rspec unit test for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,36 +1,17 @@
+# spec/models/comment_spec.rb
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  it 'is valid with a user and a post' do
-    user = User.create(name: 'Tom', post_counter: 0)
-    post = user.posts.create(title: 'My Post', comments_counter: 0, likes_counter: 0)
-    comment = post.comments.new(user:, text: 'Nice post!')
-    expect(comment).to be_valid
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:text) }
   end
 
-  it 'is not valid without a user' do
-    post = Post.create(author: User.new, title: 'My Post', comments_counter: 0, likes_counter: 0)
-    comment = post.comments.new(user: nil, text: 'Nice post!')
-    comment.valid?
-    expect(comment.errors[:user]).to include('must exist')
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
   end
 
-  it 'is not valid without text' do
-    user = User.create(name: 'Tom', post_counter: 0)
-    post = user.posts.create(title: 'My Post', comments_counter: 0, likes_counter: 0)
-    comment = post.comments.new(user:, text: nil)
-    comment.valid?
-    expect(comment.errors[:text]).to include("can't be blank")
-  end
-
-  it 'updates the comments counter for a post' do
-    user = User.create(name: 'Tom', post_counter: 1)
-    post = user.posts.create(title: 'My Post', comments_counter: 0, likes_counter: 0)
-    comment = post.comments.create(user:, text: 'Nice post!')
-
-    comment.increment_post_comments_counter
-
-    post.reload
-    expect(post.comments_counter).to eq(2)
-  end
 end
