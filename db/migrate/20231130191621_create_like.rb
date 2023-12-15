ab8fa823a7ffac3281e74f9acdc5d68a2384b560
MohammadYaser <yasersafi19@outlook.com>
2023-12-15 21:08:16 +0430

    create app views

--- db/migrate/20231130191621_create_like.rb
+++
@@ -1,9 +0,0 @@
-class CreateLike < ActiveRecord::Migration[7.1]
-  def change
-    create_table :likes do |t|
-      t.references :user, foreign_key: { to_table: :users }
-      t.references :post, foreign_key: { to_table: :posts }
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_30_191621) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_15_161843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
-    t.bigint "user_id"
-    t.bigint "post_id"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -25,8 +25,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_191621) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "post_id"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
@@ -36,8 +36,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_191621) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
     t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
