1263f5eb8379f15cfe6e006194232a230523d25c
MohammadYaser <yasersafi19@outlook.com>
2023-12-16 13:00:12 +0430

    fix linter error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,37 +1,36 @@
 class CommentsController < ApplicationController
-    # Set the associated post before certain actions
-    before_action :set_post
-  
-    # Display a form for creating a new comment
-    def new
-      @comment = Comment.new
-    end
-  
-    # Create a new comment for the associated post
-    def create
-      # Build a new comment associated with the current user and the specified post
-      @comment = current_user.comments.build(comment_params.merge(post: @post))
-  
-      # Attempt to save the comment
-      if @comment.save
-        # Redirect to the post's show page if successfully created
-        redirect_to user_post_path(@post.author, @post)
-      else
-        # Render the new comment form if there are validation errors
-        render :new
-      end
-    end
-  
-    private
-  
-    # Set the associated post based on the provided post_id
-    def set_post
-      @post = Post.find(params[:post_id])
-    end
-  
-    # Define the permitted parameters for creating a comment
-    def comment_params
-      params.require(:comment).permit(:text)
+  # Set the associated post before certain actions
+  before_action :set_post
+
+  # Display a form for creating a new comment
+  def new
+    @comment = Comment.new
+  end
+
+  # Create a new comment for the associated post
+  def create
+    # Build a new comment associated with the current user and the specified post
+    @comment = current_user.comments.build(comment_params.merge(post: @post))
+
+    # Attempt to save the comment
+    if @comment.save
+      # Redirect to the post's show page if successfully created
+      redirect_to user_post_path(@post.author, @post)
+    else
+      # Render the new comment form if there are validation errors
+      render :new
     end
   end
-  
+
+  private
+
+  # Set the associated post based on the provided post_id
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  # Define the permitted parameters for creating a comment
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,27 +1,26 @@
 class LikesController < ApplicationController
-    # Set the associated post before certain actions
-    before_action :find_post
-  
-    # Create a new like for the associated post
-    def create
-      # Build a new like associated with the current user and the specified post
-      @like = @post.likes.new(user: current_user)
-  
-      # Attempt to save the like
-      if @like.save
-        # Redirect to the post's show page with a notice if successfully created
-        redirect_to user_post_path(@post.author, @post), notice: 'You liked the post!'
-      else
-        # Redirect to the post's show page with an alert if unable to save the like
-        redirect_to user_post_path(@post.author, @post), alert: 'Unable to like the post.'
-      end
-    end
-  
-    private
-  
-    # Set the associated post based on the provided post_id
-    def find_post
-      @post = Post.find(params[:post_id])
+  # Set the associated post before certain actions
+  before_action :find_post
+
+  # Create a new like for the associated post
+  def create
+    # Build a new like associated with the current user and the specified post
+    @like = @post.likes.new(user: current_user)
+
+    # Attempt to save the like
+    if @like.save
+      # Redirect to the post's show page with a notice if successfully created
+      redirect_to user_post_path(@post.author, @post), notice: 'You liked the post!'
+    else
+      # Redirect to the post's show page with an alert if unable to save the like
+      redirect_to user_post_path(@post.author, @post), alert: 'Unable to like the post.'
     end
   end
-  
+
+  private
+
+  # Set the associated post based on the provided post_id
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+end
