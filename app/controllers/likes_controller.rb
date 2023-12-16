83fba2dc763f48b9f05c3713373b675b84669598
MohammadYaser <yasersafi19@outlook.com>
2023-12-16 17:51:02 +0430

    final changes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,26 +1,20 @@
 class LikesController < ApplicationController
-  # Set the associated post before certain actions
-  before_action :find_post
-
-  # Create a new like for the associated post
-  def create
-    # Build a new like associated with the current user and the specified post
-    @like = @post.likes.new(user: current_user)
-
-    # Attempt to save the like
-    if @like.save
-      # Redirect to the post's show page with a notice if successfully created
-      redirect_to user_post_path(@post.author, @post), notice: 'You liked the post!'
-    else
-      # Redirect to the post's show page with an alert if unable to save the like
-      redirect_to user_post_path(@post.author, @post), alert: 'Unable to like the post.'
+    def new
+      @like = Like.new
+      @user = User.find(params[:user_id])
+      @post = Post.find(params[:post_id])
+    end
+  
+    def create
+      @user = User.find(params[:user_id])
+      @post = @user.posts.find(params[:post_id])
+      @like = @post.likes.build(user: current_user)
+  
+      if @like.save
+        redirect_to user_post_path(@user, @post), notice: 'Like was successfully created.'
+      else
+        render :new
+      end
     end
   end
-
-  private
-
-  # Set the associated post based on the provided post_id
-  def find_post
-    @post = Post.find(params[:post_id])
-  end
-end
+  
