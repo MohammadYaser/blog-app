b5c589ae075260c88703489514d8b036f050cbdf
MohammadYaser <yasersafi19@outlook.com>
2023-12-23 09:13:43 +0430

    add email confirmation and reset confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   # Associations:
   # - A user has many posts where the user is the author.
   # - A user has many likes.
